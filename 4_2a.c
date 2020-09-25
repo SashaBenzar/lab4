80c0f025372d0461f6ed8070753f4d24d0f3b66e
SashaBenzar <65127188+SashaBenzar@users.noreply.github.com>
2020-09-25 13:16:19 +0300

    Update 4_2a.c

--- 4_2a.c
+++ 4_2a.c
@@ -3,25 +3,12 @@
 
 int main()
 {
-    int a = 1, i = 19, b = 1, j, c = 1, k = 19; 
+    int a = 1, i = 19; 
     while (i <= 80) {
         i++;
         if (i%5!=0) continue;
         a *= i;
     }
-    for(j=19; j <= 80; j++)
-    {
-        if (j%5!=0) continue;
-        b *= j;
-    }
-    do {
-        if (k % 5 == 0) 
-        c *= k;
-        k++;
-    } while (k <= 80);
-
     printf("while:%d\n", a);
-    printf("for:%d\n", b);
-    printf("do while:%d", c);
     return 0;
 }
